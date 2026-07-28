c26fdb804d9b2c9d9f35b17ee34c9757747d6046
Fran <fran@example.com>
2026-07-28 14:04:12 +0200

    Cambios desde el boton

--- app/src/main/java/es/guiamayores/cuidame/ServicioConsulta.kt
+++ app/src/main/java/es/guiamayores/cuidame/ServicioConsulta.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
